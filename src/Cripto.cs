7970e2a28decc146a168082d53cb83163d8fe8ae
Agentew04 <digoappelt@gmail.com>
2021-03-28 14:32:25 -0300

    add project files

---
+++ src/Cripto.cs
@@ -0,0 +1,125 @@
+﻿using PgpCore;
+using System;
+using System.IO;
+
+namespace TesteCSharp.Assistant
+{
+    public class Cripto
+    {
+        string appfolder = @Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Rodrigo's_Stuff\Ficha\";
+        public void GenKey()
+        {
+            //Definir aqui o usuario e a senha
+            string user = "username123";
+            string password = "password";
+
+            //As chaves vão ser geradas em
+            //  C:\Users\%user%\AppData\Roaming\Rodrigo's_Stuff\Ficha\
+
+
+            if(!Directory.Exists(appfolder))
+            {
+                Directory.CreateDirectory(appfolder);
+            }
+
+            PGP pgp = new PGP();
+            pgp.GenerateKey(appfolder + "public.asc", appfolder +"private.asc", user,password);
+        }
+        /// <summary>
+        /// encrypt a file based on the existing key
+        /// </summary>
+        /// <param name="inputFilePath">the path for the file to be encrypted</param>
+        /// <param name="outputFilePath">the path for the new encrypted file</param>
+        public void EncryptFile(string inputFilePath, string outputFilePath)
+        {
+            if (!File.Exists(appfolder + "public.asc"))
+            {
+                GenKey();
+            };
+            FileInfo publicKey = new FileInfo(appfolder + "public.asc");
+            EncryptionKeys encryptionKeys = new EncryptionKeys(publicKey);
+
+            FileInfo inputFile = new FileInfo(inputFilePath);
+            FileInfo outputFile = new FileInfo(outputFilePath);
+
+            PGP pgp = new PGP(encryptionKeys);
+            pgp.EncryptFile(inputFile, outputFile);
+        }
+        /// <summary>
+        /// Sign the document
+        /// </summary>
+        /// <param name="inputFilePath">The path to the file that will be signed</param>
+        /// <param name="outputFilePath">The path to the signed file</param>
+        /// <param name="password">The password for the signature</param>
+        public void SignFile(string inputFilePath, string outputFilePath, string password)
+        {
+            if (!File.Exists(appfolder + "private.asc"))
+            {
+                GenKey();
+            };
+
+            //load the keys
+            FileInfo privateKey = new FileInfo(@appfolder + "private.asc");
+            EncryptionKeys encryptionKeys = new EncryptionKeys(privateKey,password);
+
+            //input output
+            FileInfo inputFile = new FileInfo(@inputFilePath);
+            FileInfo outputFile = new FileInfo(outputFilePath);
+
+            //assinar
+            PGP pgp = new PGP(encryptionKeys);
+            pgp.SignFile(inputFile, outputFile);
+        }
+        /// <summary>
+        /// Sign a plain text string
+        /// </summary>
+        /// <param name="inputString">The string to be signed</param>
+        /// <param name="password">The password used to sign</param>
+        /// <returns>The signed string</returns>
+        public string SignString(string inputString, string password)
+        {
+            //load the key
+            FileInfo privateKey = new FileInfo(appfolder + "private.asc");
+            EncryptionKeys encryptionKeys = new EncryptionKeys(privateKey, password);
+
+            PGP pgp = new PGP(encryptionKeys);
+
+            return pgp.SignArmoredString(inputString);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputFilePath"></param>
+        /// <param name="outputFilePath"></param>
+        /// <param name="password"></param>
+        public void DecryptFile(string inputFilePath, string outputFilePath, string password)
+        {
+            //load keys
+            FileInfo privateKey = new FileInfo(appfolder + "private.asc");
+            EncryptionKeys encryptionKeys = new EncryptionKeys(privateKey, password);
+
+            //in out
+            FileInfo inputFile = new FileInfo(@inputFilePath);
+            FileInfo outputFile = new FileInfo(@outputFilePath);
+
+            //decrypt
+            PGP pgp = new PGP(encryptionKeys);
+            pgp.DecryptFile(inputFile, outputFile);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputFilePath"></param>
+        /// <returns></returns>
+        public bool VerifySign(string inputFilePath)
+        {
+            FileInfo publicKey = new FileInfo(appfolder + "public.asc");
+            EncryptionKeys encryptionKeys = new EncryptionKeys(publicKey);
+
+            FileInfo inputfile = new FileInfo(@inputFilePath);
+
+            PGP pgp = new PGP(encryptionKeys);
+            return pgp.VerifyFile(inputfile);
+        }
+    }
+}
---
+++ src/Menu.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TesteCSharp;
+using TesteCSharp.Assistant;
+
+namespace TesteCSharp
+{
+/// <summary>
+/// Lógica interna para Menu.xaml
+/// </summary>
+public partial class Menu : Window
+    {
+        Window ficha = new Windows.Ficha();
+        public Window Ficha { get => ficha; set => ficha = value; }
+        Window dados = new Dados();
+        public Window Dados { get => dados; set => dados = value; }
+        Window config = new Config();
+        public Window Config { get => config; set => config = value; }
+
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        //Abre e fecha a ficha
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Ficha.IsVisible)
+            {
+                Ficha.Hide();
+            } else
+            {
+                Ficha.Show();
+            }
+        }
+
+        //Força a fechada do aplicativo
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Saving s = new Saving();
+            s.SaveToDisk(s.CreateJSONFile());
+            //SaveConfig();
+            Environment.Exit(0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Dados.IsVisible)
+            {
+                Dados.Hide();
+            }
+            else
+            {
+                Dados.Show();
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (Config.IsVisible)
+            {
+                Config.Hide();
+            }
+            else
+            {
+                Config.Show();
+            }
+        }
+    }
+}
