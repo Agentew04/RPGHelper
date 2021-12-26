10a3c17e9647ab3990bf4a5adb81686d1b876c98
Rodrigo Appelt <digoappelt@gmail.com>
2021-12-26 01:44:40 -0300

    add pages

---
+++ src/Ficha/Models/Skills.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.SmartEnum;
+
+namespace Ficha.Models
+{
+    public sealed class Skills : SmartEnum<Skills>
+    {
+        public static readonly Skills None = new("None", 0, "Nada", "NUL");
+        public static readonly Skills Dexterity = new("Dexterity", 1, "Destreza", "DES");
+        public static readonly Skills Strength = new("Strength", 2, "Força", "FOR");
+        public static readonly Skills Vitality = new("Vitality", 3, "Vitalidade", "VIT");
+        public static readonly Skills Intelligence = new("Intelligence", 4, "Inteligência", "INT");
+        public static readonly Skills Speak = new("Speak", 5, "Lábia", "LAB");
+        public static readonly Skills Wisdom = new("Wisdom", 6, "Sabedoria", "SAB");
+
+        public string UserFriendlyName { get; }
+        public string Code { get; }
+
+        public Skills(string name, int value, string userFriendlyName, string code) : base(name, value)
+        {
+            UserFriendlyName=userFriendlyName;
+            Code=code;
+        }
+        public static Skills FromCode(string code)
+        {
+            return code switch
+            {
+                "NUL" => None,
+                "DES" => Dexterity,
+                "FOR" => Strength,
+                "VIT" => Vitality,
+                "INT" => Intelligence,
+                "LAB" => Speak,
+                "SAB" => Wisdom,
+                _ => None,
+            };
+        }
+    }
+}
---
+++ src/Ficha/Pages/FichaPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Ficha.Models;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Ficha.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FichaPage : Page
+    {
+        public FichaPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+            BackpackItems = new();
+            backPackListView.ItemsSource = BackpackItems;
+            this.ItemRadioButtonSelected(null, null);
+        }
+
+        public List<BackpackItem> BackpackItems { get; set; }
+
+        private void ItemRadioButtonSelected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ArmaRadioButtonSelected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
